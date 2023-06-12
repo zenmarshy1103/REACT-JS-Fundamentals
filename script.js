c979c7931412fe9f8a0baae140333f5316eb41fa
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-06-13 08:47:44 +1200

    Working with Immutable Arrays (Techniques)
    //  - Always create a new away using [...] spread operator for array or {...} object to not change the original data structure
    //  - <Techniques>
    //    - 1. Adding elements without changing the original array
    //    - 2. Delete elements without changing the original array
    //    - 3. Update elements without changing the original array
    //  - <THEORY>
    //    - In React, many operations need to be immutable (Where we do operation to not manipulate the underlying data structure )
    //    - In React always good to leave original array, so make a copy of the original array first

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// The Array Sort Method
-//  - (NOT Functional Array Method)
-//    - Mutates the original array
+// Working with Immutable Arrays (Techniques)
+//  - Always create a new away using [...] spread operator for array or {...} object to not change the original data structure
+//  - <Techniques>
+//    - 1. Adding elements without changing the original array
+//    - 2. Delete elements without changing the original array
+//    - 3. Update elements without changing the original array
 //  - <THEORY>
-//    - Loop over an array, return a new array with one value
-//    - In React always good to leave original array, so make a copy of the original array first using .slice() then perform .sort()
-//  - <SYNTAX>
-//    [arrElement].slice().sort(callbackfunction(currentElementValue, nextElementValue ) )
-//  when the return value of the call back function is:
-//    - negative the values will be sorted in ascending way (a - b)
-//    - positive the values will be sorted in descending way (b - a)
+//    - In React, many operations need to be immutable (Where we do operation to not manipulate the underlying data structure )
+//    - In React always good to leave original array, so make a copy of the original array first
 
 const data = [
   {
@@ -252,14 +250,52 @@ console.log(pagesAllBooks); // 2nd iteration accumulator is 1216, after this ite
 
 const x = [3, 7, 1, 9, 6];
 // Using .slice() to make a copy of the original array to the new array variable and work with it without mutating the original array
-const sorted = x.slice().sort((a, b) => a - b);   //a is the currentElementValue of array and b is the nextElementValue of the array
+const sorted = x.slice().sort((a, b) => a - b); //a is the currentElementValue of array and b is the nextElementValue of the array
 // when the return value of the call back function is:
 //  - negative the values will be sorted in ascending way (a - b)
 //  - positive the values will be sorted in descending way (b - a)
-console.log(x)
+console.log(x);
 console.log(sorted);
 
 // Applying .sort() on object elements in the array (Use Case for React and Real World Scenario)
 // Sorting the book array with object element from book with longest pages to shortest pages (Ascending Way)
-const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages)  // a and b gets the value of the array , since we are using book array's pages property in the array to arrange the object element in the array
-console.log(sortedByPages);  
+const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages); // a and b gets the value of the array , since we are using book array's pages property in the array to arrange the object element in the array
+console.log(sortedByPages);
+
+// >> 5. Working with Immutable Arrays (Techniques)
+//  - <Techniques>
+//    - 1. Adding elements without changing the original array
+//    - 2. Delete elements without changing the original array
+//    - 3. Update elements without changing the original array
+//  - <THEORY>
+//    - In React, many operations need to be immutable (Where we do operation to not manipulate the underlying data structure )
+//    - In React always good to leave original array, so make a copy of the original array first using .slice() then perform .sort()
+
+// >> 1. Add Book Object to array
+const newBook = {
+  id: 6,
+  title: "Harry Porter and the Chamber of Secrets",
+  arthor: "J. K Rowling",
+};
+
+// Adding new object onto array
+// - using spread operator [...originalArr]  (Creates a mew array )
+const booksAfterAdd = [...books, newBook]; //Adding the object element at the end of the new array
+console.log(books);
+console.log(booksAfterAdd);
+
+// >> 2. Delete Book Object from an array
+
+// Delete book object with id 3 using filter() to filter it out of the array
+//  - filter() creates a new array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3); //book ID 3 is filtered out of the array at it returns false for the condition only elements that return true will be put on (filtered in) the array
+console.log(booksAfterDelete);
+
+// >> 3. Update Book Object from an array
+
+// Update / Change the book object that is id 1 using .map() and return value using ternary operation
+// using spread operator on object, spreads all the properties of the object and just updating one property of the object element
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...books, pages: 1 } : book
+); // true: update the page property, false: return current book object element
+console.log(booksAfterUpdate);
